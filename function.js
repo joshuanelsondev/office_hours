162cdfcdd2b99db526ccc844603cd3028024eb25
joshuanelsondev <joshuanelsondev@gmail.com>
2024-01-18 22:48:43 -0500

    Remove files

--- function.js
+++
@@ -1,7 +0,0 @@
-let name = prompt("What is your name?");
-function capitalizeInput() {
-  let capitalizeName = name[0].toUpperCase() + name.slice(1).toLowerCase();
-  return `Hello ${capitalizeName}`;
-}
-
-capitalizeInput();
